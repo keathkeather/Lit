39606e4e28b91f3cb01f7176f79b6f5a2747a1e5
shaniacc <canoy.shania@gmail.com>
2023-11-23 12:38:03 +0800

    Made reusable Header for navigation

--- frontend/src/Landing/FeaturesScreen.tsx
+++ frontend/src/Landing/FeaturesScreen.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header';
 
 interface FeaturesScreenProps {}
 
 const FeaturesScreen: React.FC<FeaturesScreenProps> = () => {
-  const navigate = useNavigate();
-
-    const handleLogin = () => {
-        navigate('/login');
-    };
-
-    const handleSignup = () => {
-        navigate('/signup');
-    };
-
-    const handleLogoClick = () => {
-        navigate('/');
-      };
 
   return (
     <div className="relative overflow-y-auto">
@@ -27,46 +14,7 @@ const FeaturesScreen: React.FC<FeaturesScreenProps> = () => {
         }
       `}
     </style>
-        <header className="bg-white fixed top-0 left-0 right-0 z-10 p-2 md:p-4 flex flex-row lg:flex-row items-center justify-between">
-            <div className="flex items-center mb-0 ml-20">
-                <button
-                    onClick={handleLogoClick}
-                >
-                    <img src="litimg/litlogo2.png" alt="Lit Logo 2" className="w-20 lg:w-28 mr-3 lg:mr-5" />
-                </button>
-                <Link to="/landing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Home
-                </Link>
-                <Link to="/features" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Features
-                </Link>
-                <Link to="/pricing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Pricing
-                </Link>
-                <Link to="/aboutus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    About Us
-                </Link>
-                <Link to="/contactus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Contact Us
-                </Link>
-            </div>
-            <div className="flex items-center mr-20">
-                <button
-                    type="button"
-                    onClick={handleLogin}
-                    className="px-3 py-1 lg:px-4 lg:py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer mb-0 lg:text-lg"
-                >
-                    Log in
-                </button>
-                <button
-                    type="button"
-                    onClick={handleSignup}
-                    className="mx-10 px-3 py-1 ml-2 lg:ml-5 lg:px-4 lg:py-2 rounded bg-bgc2 text-white font-semibold cursor-pointer mb-0 lg:text-lg"
-                >
-                    Sign up for Free
-                </button>
-            </div>
-        </header>
+       <Header/>
 
         <div className=" mt-16 p-4 text-center">
       {/* Add your headings here */}
@@ -117,7 +65,7 @@ const FeaturesScreen: React.FC<FeaturesScreenProps> = () => {
     </div>
 
     {/* Fixed bottom image */}
-      <img id="bottomImage" src="litimg/bgfeatures.png" alt="Image Description" className="w-screen h-[350px] object-cover" />
+      <img id="bottomImage" src="litimg/bgfeatures.png" alt="Description" className="w-screen h-[350px] object-cover" />
 
 </div>
 );
--- frontend/src/Landing/PricingScreen.tsx
+++ frontend/src/Landing/PricingScreen.tsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import Header from './Header'
 
 interface PricingScreenProps {}
 
 const PricingScreen: React.FC<PricingScreenProps> = () => {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate('/login');
-  };
-
-  const handleSignup = () => {
-    navigate('/signup');
-  };
-
-  const handleLogoClick = () => {
-    navigate('/');
-  };
-
   return (
     <div className="overflow-y-auto" style={{ paddingTop: '64px' }}>
       <style>
@@ -32,46 +18,8 @@ const PricingScreen: React.FC<PricingScreenProps> = () => {
           }
         `}
       </style>
-      <header className="bg-white fixed top-0 left-0 right-0 z-10 p-2 md:p-4 flex flex-row lg:flex-row items-center justify-between">
-            <div className="flex items-center mb-0 ml-20">
-                <button
-                    onClick={handleLogoClick}
-                >
-                    <img src="litimg/litlogo2.png" alt="Lit Logo 2" className="w-20 lg:w-28 mr-3 lg:mr-5" />
-                </button>
-                <Link to="/landing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Home
-                </Link>
-                <Link to="/features" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Features
-                </Link>
-                <Link to="/pricing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Pricing
-                </Link>
-                <Link to="/aboutus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    About Us
-                </Link>
-                <Link to="/contactus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Contact Us
-                </Link>
-            </div>
-            <div className="flex items-center mr-20">
-                <button
-                    type="button"
-                    onClick={handleLogin}
-                    className="px-3 py-1 lg:px-4 lg:py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer mb-0 lg:text-lg"
-                >
-                    Log in
-                </button>
-                <button
-                    type="button"
-                    onClick={handleSignup}
-                    className="mx-10 px-3 py-1 ml-2 lg:ml-5 lg:px-4 lg:py-2 rounded bg-bgc2 text-white font-semibold cursor-pointer mb-0 lg:text-lg"
-                >
-                    Sign up for Free
-                </button>
-            </div>
-        </header>
+     
+     <Header/>
 
       {/* Pricing section */}
       <section className="mt-16 flex justify-center items-center h-full relative z-10">
