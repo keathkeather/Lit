2af6ba635f0c6ec8e16eb73723a66708b75208ce
shaniacc <canoy.shania@gmail.com>
2023-11-28 23:36:26 +0800

    made nav bar smaller

--- frontend/src/Landing/Header.tsx
+++ frontend/src/Landing/Header.tsx
@@ -19,41 +19,60 @@ const Header: React.FC<HeaderProps> = () => {
   };
 
   return (
-    <header className="bg-white fixed top-0 left-0 right-0 z-10 p-2 md:p-4 flex flex-row lg:flex-row items-center justify-between">
-      <div className="flex items-center mb-0 ml-20">
+    <header className="bg-white fixed top-0 left-0 right-0 z-10 p-2 md:p-4 flex flex-row lg:flex-row items-center justify-between shadow-lg">
+      <div className="flex items-center lg:ml-0">
         <button onClick={handleLogoClick}>
-          <img src="litimg/litlogo2.svg" alt="Lit Logo 2" className="w-20 lg:w-28 mr-3 lg:mr-5" />
+          <img src="litimg/litlogo2.svg" alt="Lit Logo 2" className="w-12 lg:w-20 md:w-40 mr-2 lg:ml-20" />
         </button>
-        <Link to="/landing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Home
-                </Link>
-                <Link to="/features" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Features
-                </Link>
-                <Link to="/pricing" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Pricing
-                </Link>
-                <Link to="/aboutus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    About Us
-                </Link>
-                <Link to="/contactus" className="text-lg lg:text-xl font-bold text-gray mr-5 md:mr-7">
-                    Contact Us
-                </Link>
+          <ul className="flex">
+            <li>
+              <Link
+                to="/landing"
+                className="text-base lg:text-lg font-bold text-gray mr-3 md:mr-4 hover:text-gray-800"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/features"
+                className="text-base lg:text-lg font-bold text-gray mr-3 md:mr-4 hover:text-gray-800"
+              >
+                Features
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/pricing"
+                className="text-base lg:text-lg font-bold text-gray mr-3 md:mr-4 hover:text-gray-800"
+              >
+                Pricing
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/aboutus"
+                className="text-base lg:text-lg font-bold text-gray mr-3 md:mr-4 hover:text-gray-800"
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/contactus"
+                className="text-base lg:text-lg font-bold text-gray hover:text-gray-800"
+              >
+                Contact Us
+              </Link>
+            </li>
+          </ul>
       </div>
       <div className="flex items-center">
-        <button
-          type="button"
-          onClick={handleLogin}
-          className="px-3 py-1 lg:px-4 lg:py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer mb-0 lg:text-lg"
-        >
-          Log in
+        <button onClick={handleLogin}>
+          <img src="litimg/loginbtn.svg" alt="loginbtn" className="w-12 lg:w-40 mr-2 lg:mr-3" />
         </button>
-        <button
-          type="button"
-          onClick={handleSignup}
-          className="mx-10 px-3 py-1 ml-2 lg:ml-5 lg:px-4 lg:py-2 rounded bg-bgc2 text-white font-semibold cursor-pointer mb-0 mr-20 lg:text-lg"
-        >
-          Sign up for Free
+        <button onClick={handleSignup}>
+          <img src="litimg/signup_forfreebtn.svg" alt="signupbtn" className="w-12 lg:w-40 mr-2 lg:mr-14`" />
         </button>
       </div>
     </header>
