10a71e2df432d42121cd5e228cd6605b08733234
keathkeather <67945664+keathkeather@users.noreply.github.com>
2023-12-02 07:19:24 +0800

    added account booklistt

---
+++ backend/backend/src/main/java/com/CSIT321/backend/Controller/BookListController.java
@@ -0,0 +1,73 @@
+package com.CSIT321.backend.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.CSIT321.backend.Service.BookListService;
+import com.CSIT321.backend.Entity.BookEntity;
+import com.CSIT321.backend.Entity.BookListEntity;
+
+@RestController
+@RequestMapping("/booklist")
+public class BookListController {
+
+    @Autowired
+    BookListService bookListService;
+
+    @PostMapping("/create")
+    public ResponseEntity<BookListEntity> createBookList(@RequestBody BookListEntity list) {
+        try {
+            BookListEntity createdList = bookListService.createList(list);
+            return new ResponseEntity<>(createdList, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    @PostMapping("/addBook/{accountId}")
+    public ResponseEntity<BookListEntity> addBookToList(@PathVariable int accountId, @RequestParam int bookId){
+        try{
+            BookListEntity bookList = bookListService.addBookToList(accountId, bookId);
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        }catch(Exception e){
+            throw e;
+        }
+    }
+    @GetMapping("/getAllBookInList/{accountId}")
+    public ResponseEntity<List<BookEntity>> getAllBooksInList(@PathVariable int accountId){
+        try{
+            List<BookEntity> bookList = bookListService.getAllBooksInList(accountId);
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        }catch(Exception e){
+            throw e;
+        }
+    }
+    @PutMapping("/addBook/{accountId}")
+    public ResponseEntity<BookListEntity> addBooktoList(@PathVariable int accountId, @RequestParam int bookId){
+        try{
+            BookListEntity bookList = bookListService.addBookToList(accountId, bookId);
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        }catch(Exception e){
+            throw e;
+        }
+    }
+    @PutMapping("/removeBook/{accountId}")
+    public ResponseEntity<BookListEntity> removeBookfromList(@PathVariable int accountId, @RequestParam int bookId){
+        try{
+            BookListEntity bookList = bookListService.removeBookFromList(accountId, bookId);
+            return new ResponseEntity<>(bookList, HttpStatus.OK);
+        }catch(Exception e){
+            throw e;
+        }
+    }
+}
---
+++ backend/backend/src/main/java/com/CSIT321/backend/Entity/BookListEntity.java
@@ -0,0 +1,64 @@
+package com.CSIT321.backend.Entity;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import java.util.List;
+
+@Table(name = "booklist")
+@Entity
+public class BookListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int bookListId;
+
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    @JsonBackReference
+    private AccountEntity account;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "book_list_id")
+    private List<BookEntity> books;
+
+    public BookListEntity() {
+        super();
+    }
+    public BookListEntity(AccountEntity account) {
+        this.account = account;
+    }
+
+    public BookListEntity(AccountEntity account, List<BookEntity> books) {
+        this.account = account;
+        this.books = books;
+    }
+    public void addBook(BookEntity book) {
+        if (book != null) {
+            books.add(book);
+        }
+    }
+
+    public void removeBook(BookEntity book) {
+        if (book != null) {
+            books.remove(book);
+        }
+    }
+
+    public AccountEntity getAccount() {
+        return this.account;
+    }
+
+    public List<BookEntity> getBooks() {
+        return this.books;
+    }
+
+    public void setAccount(AccountEntity account) {
+        this.account = account;
+    }
+
+    public void setBooks(List<BookEntity> books) {
+        this.books = books;
+    }
+}
