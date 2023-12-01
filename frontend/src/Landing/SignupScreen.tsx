5a4e34e77f9a38e9dbf3a2f51f8993a135aa91a9
shaniacc <canoy.shania@gmail.com>
2023-12-02 00:20:19 +0800

    Integrated backend and frontend for Sign up

--- frontend/src/Landing/SignupScreen.tsx
+++ frontend/src/Landing/SignupScreen.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import PasswordChecklist from "react-password-checklist";
+
+import axios from 'axios'
 
 interface SignupScreenProps {}
 
@@ -11,13 +14,48 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [genderError, setGenderError] = useState('');
+  const [showPasswordChecklist, setShowPasswordChecklist] = useState(false); // New state to manage checklist visibility
 
   const navigate = useNavigate();
 
-  const handleSignup = () => {
-    // Add logic to handle user registration
-    console.log('Signup succesful');
-    navigate('/checkpoint');
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setShowPasswordChecklist(e.target.value.length > 0); // Show checklist when the user starts typing in the password field
+  };
+
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Perform form validation
+    if (!firstName || !lastName || !username || !email || !password || !confirmPassword) {
+      alert("Please fill in all required fields.");
+      return;
+    }
+
+    if (!gender) {
+      setGenderError('Please select a gender.');
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:8080/user/createUser", {
+        "username": username,
+        "password": password,
+        "account": {
+          "email": email,
+          "firstName": firstName,
+          "lastName": lastName,
+          "gender": gender
+        }
+      });
+
+      console.log(response.data); // Log the response if needed
+      alert("User Registration Successful!");
+      navigate('/checkpoint');
+    } catch (err) {
+      alert(err);
+    }
   };
 
   const handleLogoClick = () => {
@@ -32,7 +70,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
       <img src="litimg/signupbg.png" alt="Signup Background" className="w-full h-full object-cover"/>
 
       <div className="absolute top-64 mt-80 left-1/4 mr-40 transform translate-x-1/2 -translate-y-1/2 bg-white p-10 rounded-md max-w-2xl w-full">
-        <form>
+        <form onSubmit={handleSignup}>
           <div className="mb-4 flex">
             <div className="mr-2 w-1/2">
               <label htmlFor="firstName" className="block text-gray text-xl font-bold mb-2">
@@ -44,6 +82,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                 className="w-full border rounded-md py-2 px-2 mb-2 text-lg"
                 onChange={(e) => setFirstName(e.target.value)}
                 value={firstName}
+                required
               />
             </div>
             <div className="ml-2 w-1/2">
@@ -56,6 +95,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                 className="w-full border rounded-md py-2 px-2 mb-2 text-lg"
                 onChange={(e) => setLastName(e.target.value)}
                 value={lastName}
+                required
               />
             </div>
           </div>
@@ -69,6 +109,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
               className="w-full border rounded-md py-2 px-2 mb-4 text-lg"
               onChange={(e) => setUsername(e.target.value)}
               value={username}
+              required
             />
           </div>
           <div className="mb-4 flex">
@@ -82,7 +123,10 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                     name="gender"
                     value="male"
                     checked={gender === 'male'}
-                    onChange={() => setGender('male')}
+                    onChange={() => {
+                      setGender('male');
+                      setGenderError(''); 
+                    }}
                     className="mr-2"
                   />
                   <label htmlFor="male" className="text-gray text-xl">
@@ -96,7 +140,10 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                     name="gender"
                     value="female"
                     checked={gender === 'female'}
-                    onChange={() => setGender('female')}
+                    onChange={() => {
+                      setGender('female');
+                      setGenderError(''); // Clear error when a gender is selected
+                    }}
                     className="mr-2"
                   />
                   <label htmlFor="female" className="text-gray text-xl">
@@ -110,7 +157,10 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                     name="gender"
                     value="nonBinary"
                     checked={gender === 'nonBinary'}
-                    onChange={() => setGender('nonBinary')}
+                    onChange={() => {
+                      setGender('male');
+                      setGenderError(''); // Clear error when a gender is selected
+                    }}
                     className="mr-2"
                   />
                   <label htmlFor="nonBinary" className="text-gray text-xl">
@@ -118,6 +168,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
                   </label>
                 </div>
               </div>
+              {genderError && <p className="text-[#ff3333]">{genderError}</p>}
             </div>
           </div>
           <div className="mb-4">
@@ -130,6 +181,7 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
               className="w-full border rounded-md py-2 px-2 mb-4 text-lg"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
+              required
             />
           </div>
           <div className="mb-4">
@@ -140,8 +192,9 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
               type="password"
               id="password"
               className="w-full border rounded-md py-2 px-2 mb-4 text-lg"
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={handlePasswordChange}
               value={password}
+              required
             />
           </div>
           <div className="mb-4">
@@ -151,19 +204,27 @@ const SignupScreen: React.FC<SignupScreenProps> = () => {
             <input
               type="password"
               id="confirmPassword"
-              className="w-full border rounded-md py-2 px-2 mb-4 text-lg"
+              className="w-full border rounded-md py-2 px-2 mb-2 text-lg"
               onChange={(e) => setConfirmPassword(e.target.value)}
               value={confirmPassword}
+              required
             />
+          {showPasswordChecklist && (
+          <PasswordChecklist
+            rules={['capital', 'match', 'specialChar', 'minLength', 'number']}
+            minLength={8}
+            value={password}
+            valueAgain={confirmPassword}
+          />
+        )}
           </div>
           <div className="block text-dblue text-lg font-normal mb-10">
             By clicking the button below, you are indicating that you have read and agree to the Terms of Service and Privacy Policy.
           </div>
           <div className="flex flex-col items-center">
             <button
-              type="button"
+              type="submit"
               className="bg-dblue text-white text-xl font-semibold py-4 px-40 rounded-md hover:bg-dblue mb-4"
-              onClick={handleSignup}
             >
               Create
             </button>
