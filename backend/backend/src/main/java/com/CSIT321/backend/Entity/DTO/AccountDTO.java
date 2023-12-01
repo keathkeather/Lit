bedaa585854be0a1db3c8daddf0522a8f2de4518
keathkeather <67945664+keathkeather@users.noreply.github.com>
2023-12-01 16:50:12 +0800

    added accountDto

--- backend/backend/src/main/java/com/CSIT321/backend/Entity/DTO/AccountDTO.java
+++ backend/backend/src/main/java/com/CSIT321/backend/Entity/DTO/AccountDTO.java
@@ -1,44 +1,94 @@
 package com.CSIT321.backend.Entity.DTO;
 
 public class AccountDTO {
-    private String email;
-    
-    private UserDTO user;  // Include a UserDTO field
-    private RoleDTO role;
 
+    private int accountId;
+    private int userId; // Assuming you want to include the user ID in the DTO
+    private int roleId; // Assuming you want to include the role ID in the DTO
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private boolean isDeleted;
 
     public AccountDTO() {
-        // default constructor
+        // Default constructor
     }
 
-    public AccountDTO(String email, UserDTO user, RoleDTO role) {
+    public AccountDTO(int accountId, int userId, int roleId, String email, String firstName, String lastName, String gender, boolean isDeleted) {
+        this.accountId = accountId;
+        this.userId = userId;
+        this.roleId = roleId;
         this.email = email;
-        this.user = user;
-        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.isDeleted = isDeleted;
+    }
 
-    }
-    public RoleDTO getRole() {
-        return role;
-    }
-    
     // Getters and setters
-    
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
     public String getEmail() {
         return email;
-    }
-    public void setRole(RoleDTO role) {
-        this.role = role;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public UserDTO getUser() {
-        return user;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setUser(UserDTO user) {
-        this.user = user;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 }
