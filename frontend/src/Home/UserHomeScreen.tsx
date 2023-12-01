bdbbc2b98c38136d074292832ec2290e5fa4a158
Rynze-RJ <rynzerj.l@gmail.com>
2023-12-02 05:21:21 +0800

    edited UserHome

--- frontend/src/Home/UserHomeScreen.tsx
+++ frontend/src/Home/UserHomeScreen.tsx
@@ -54,7 +54,7 @@ const UserHomeScreen: React.FC = () => {
           <img src="litimg/home.png" alt="Home" className="w-screen h-full" />
           <div className="absolute top-1/2 left-1/3 transform -translate-x-1/2 -translate-y-1/2">
             <div className="flex items-center"
-            style={{ width: '640px' }}>
+            style={{ width: '620px', marginRight: '100px' }}>
               <img src="litimg/litsy.png" alt="Litsy" className="w-40 mr-4" />
               {user && (
               <div className="text-white text-3xl font-semibold mt-2">Welcome, {user.username}! May you have a wondrous adventure to the World of Filipino Literature</div>
