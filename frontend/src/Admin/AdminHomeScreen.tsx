c1e80148f7579cab9c277c13dad076b121c64db2
rastixylayax <ylayarusticojohn@gmail.com>
2023-12-04 02:27:12 +0800

    added adminhomescreen and sidebar

--- frontend/src/Admin/AdminHomeScreen.tsx
+++ frontend/src/Admin/AdminHomeScreen.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Sidebar from './Sidebar';
 
 interface RoleEntity {
   role_id: number;
@@ -48,33 +49,51 @@ const AdminHomeScreen: React.FC<AdminHomeScreenProps> = () => {
   }, [allUsersFetched]);
 
   return (
-    <div className=''>
-      <h1>Hello admin</h1>
-      <div className = "relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className ="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">Username</th>
-              <th scope="col" className="px-6 py-3">Email</th>
-              <th scope="col" className="px-6 py-3">Role</th>
-              <th scope="col" className="px-6 py-3">
-                    <span className="sr-only">Edit</span>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 ml-64 p-4">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Username
                 </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.account.accountId} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black">{user.username}</th>
-                <td className="px-6 py-4">{user.account.email}</td>
-                <td>{user.account.role.role_name}</td>
-                <td className="px-6 py-4 text-right">
-                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Role
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Edit</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr
+                  key={user.account.accountId}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-black"
+                  >
+                    {user.username}
+                  </th>
+                  <td className="px-6 py-4">{user.account.email}</td>
+                  <td className="px-6 py-4"> {user.account.role.role_name} </td>
+                  <td className="px-6 py-4 text-right">
+                    <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
