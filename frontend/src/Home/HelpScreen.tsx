f1bf3d71962f09684c8da48995a2dd7ab337f203
Rynze-RJ <rynzerj.l@gmail.com>
2023-12-05 21:47:04 +0800

    Help done

--- frontend/src/Home/HelpScreen.tsx
+++ frontend/src/Home/HelpScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header'
-import { useLocation, useParams  } from 'react-router-dom';
+import { useParams  } from 'react-router-dom';
 import Accordion from './Accordion';
 import axios from 'axios';
 
@@ -12,23 +12,24 @@ interface AccountEntity{
   gender: string;
 }
 
-interface FeedbackEntity{
-  account: AccountEntity;
-  feedbackText: string;
-}
-
 const HelpScreen: React.FC = () => {
   const [loading, setLoading] = useState(true);
   const [account, setAccount] = useState<AccountEntity>();
-  const location = useLocation();
   const { accountId } = useParams<{ accountId: string }>();
   const numAccountId = Number(accountId);
-  const [showFeedbackForm, setShowFeedbackForm] = useState(false);
   const [feedbackText, setFeedbackText] = useState('');
+  const [reportText, setReportText] = useState('');
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [isPModalVisible, setPModalVisible] = useState(false);
+  const [isFModalVisible, setFModalVisible] = useState(false);
+  const [isSModalVisible, setSModalVisible] = useState(false);
+  const [reportError, setReportError] = useState('');
+  const [feedbackError, setFeedbackError] = useState('');
+  const allModalVisible = isModalVisible || isPModalVisible || isFModalVisible || isSModalVisible;
 
   useEffect(() => {
     const getUserData = async () => {
-      console.log(`i am on the help page ${numAccountId}`)
+      console.log(`i am on the help page ${numAccountId}`);
       try {
         if (numAccountId !== undefined) {
           const response = await axios.get<AccountEntity>(`http://localhost:8080/account/${numAccountId}`);
@@ -54,19 +55,13 @@ const HelpScreen: React.FC = () => {
     { question: '04\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0Does this include folklore stories in the Philippines?', answer: 'Absolutely! Folklore is a distinct and captivating genre within Lit. We take pride in offering a collection of stories that delve into the rich and enchanting world of Philippine folklore. Readers can explore tales inspired by local myths, legends, and traditions, providing a unique and culturally immersive reading experience. Discover the magic and wonder of Philippine folklore right here on Lit!' },
   ];
 
-  const openFeedbackForm = () => {
-    setShowFeedbackForm(true);
-  };
-
-  const closeFeedbackForm = () => {
-    setShowFeedbackForm(false);
-    setFeedbackText('');
-  };
-
   const handleSendFeedback = async () => {
-    if (!feedbackText) {
-      alert('Please provide feedback before sending.');
+    if(loading){
+      alert('Loading...');
       return;
+    }
+    if(account){
+      console.log(account);
     }
   
     const jsonData = {
@@ -88,26 +83,138 @@ const HelpScreen: React.FC = () => {
       console.log('Response:', response);
   
       if (response.ok) {
+        setFeedbackText('');
         const data = await response.json();
         console.log('Feedback created successfully:', data);
-        closeFeedbackForm();
       } else {
-        closeFeedbackForm();
         console.error('Error creating feedback:', response.statusText);
-        //alert('An error occurred while sending feedback.');
+        alert('An error occurred while sending feedback.');
       }
     } catch (error) {
-      closeFeedbackForm();
       console.error('Error creating feedback:', error);
-      //alert('An error occurred while sending feedback.');
+      alert('An error occurred while sending feedback.');
     }
   };  
 
+  const handleSendReport = async () => {
+    if(loading){
+      alert('Loading...');
+      return;
+    }
+    if(account){
+      console.log(account);
+    }
+  
+    const jsonData = {
+      account: {
+        accountId: numAccountId,
+      },
+      report: reportText,
+    };
+  
+    try {
+      const response = await fetch('http://localhost:8080/report/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonData),
+      });
+  
+      console.log('Response:', response);
+  
+      if (response.ok) {
+        setReportText('');
+        const data = await response.json();
+        console.log('Report created successfully:', data);
+      } else {
+        console.error('Error creating feedback:', response.statusText);
+        alert('An error occurred while sending report.');
+      }
+    } catch (error) {
+      console.error('Error creating feedback:', error);
+      alert('An error occurred while sending report.');
+    }
+  };  
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+    const modal1 = document.getElementById('popup-modal');
+    setReportError('');
+    setFeedbackError('');
+    if(isPModalVisible === true){
+      togglePModal();
+    }
+    if(isFModalVisible === true){
+      toggleFModal();
+    }
+    if(isSModalVisible === true){
+      toggleSModal();
+    }
+    if (modal1) {
+      modal1.classList.toggle('hidden');
+    }
+  };  
+
+  const togglePModal = () => {
+    setPModalVisible(!isPModalVisible);
+    const modal = document.getElementById('popup-pmodal');
+    if (modal) {
+      modal.classList.toggle('hidden');
+    }
+  };  
+
+  const toggleFModal = () => {
+    setFModalVisible(!isFModalVisible);
+    const modal = document.getElementById('popup-fmodal');
+    if (modal) {
+      modal.classList.toggle('hidden');
+    }
+  };  
+
+  const toggleSModal = () => {
+    setSModalVisible(!isSModalVisible);
+    const modal = document.getElementById('popup-smodal');
+    if(isPModalVisible === true){
+      togglePModal();
+    }
+    if(isFModalVisible === true){
+      toggleFModal();
+    }
+    if (modal) {
+      modal.classList.toggle('hidden');
+    }
+  };   
+
   return (
-    <div className="overflow-y-auto">
+    <div className={`overflow-y-auto ${allModalVisible ? 'modal-open' : ''}`}>
 
         <Header/>
-        
+        {allModalVisible && <div className="overlay"></div>}
+        <style>
+          {`
+            .overlay {
+              position: fixed;
+              top: 0;
+              left: 0;
+              width: 100%;
+              height: 100%;
+              background-color: rgba(0, 0, 0, 0.5);
+              z-index: 10;
+            }
+
+            .modal {
+              position: fixed;
+              right: 1%;
+              bottom: 2%;
+              z-index: 11;
+            }
+
+            .modal-open {
+              overflow: hidden;
+            }
+          `}
+        </style>
         <div className="flex flex-col items-center justify-center mt-16">
             
           <div className="relative">
@@ -135,34 +242,166 @@ const HelpScreen: React.FC = () => {
             ))}
           </div>
 
-          <div className="fixed bottom-7 right-7">
-            {showFeedbackForm && (
-              <div className="feedback-form mt-20 flex flex-col items-center bg-lightg border border-dblue p-5 rounded-md">
-                <h2 className="text-dblue font-semibold text-lg mb-3">Provide Feedback</h2>
-                <textarea
-                  placeholder={`Type your feedback here...`}
-                  value={feedbackText}
-                  className="border rounded-md py-2 px-2 mb-2 text-sm"
-                  onChange={(e) => setFeedbackText(e.target.value)}
-                />
-                <div className="flex flex-row mt-3">
-                  <button onClick={handleSendFeedback} type="button"
-                          className="mr-2 px-5 py-1 rounded bg-bgc1 text-white font-semibold cursor-pointer text-sm">
-                    Send</button>
-                  <button onClick={closeFeedbackForm} type="button"
-                          className="px-3 py-1 rounded bg-bgc2 text-white font-semibold cursor-pointer text-sm">
-                    Cancel</button>
-                </div>
-              </div>
-            )}
-          </div>
-
           <div className="mt-20 mb-20">
-            <button onClick={openFeedbackForm}>
+            <button onClick={toggleModal}>
               <img src="/litimg/feedbackbtn.svg" alt="feedbackbtn" className="w-80" />
             </button>
           </div>
 
+          <div
+            id="popup-modal"
+            className={`modal ${isModalVisible ? '' : 'hidden'}`}
+          >
+            <div className="w-full max-w-md max-h-full">
+              <div className="relative bg-white border border-white rounded-lg">
+                <div className="text-bgc1 font-bold text-lg text-center mt-3 mb-2">Leave your feedback!</div>
+                <button type="button" onClick={toggleModal} className="absolute top-3 right-3">
+                  <img src="/litimg/x.png" alt="feedbacksbtn" className="w-7" />
+                </button>
+                <div className="p-4 md:p-5 text-center">
+                  <div className="flex flex-row mb-6">
+                    <button className="mr-3" onClick={() => { toggleModal(); togglePModal(); }}>
+                      <img src="/litimg/problemsbtn.svg" alt="feedbacksbtn" className="w-30" />
+                    </button>
+                    <button onClick={() => { toggleModal(); toggleFModal(); }}>
+                      <img src="/litimg/feedbacksbtn.svg" alt="problemsbtn" className="w-30" />
+                    </button>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            id="popup-pmodal"
+            className={`modal ${isPModalVisible ? '' : 'hidden'}`}
+          >
+            <div className="w-full max-w-md max-h-full">
+              <div className="relative bg-white border border-white rounded-lg flex flex-col items-center"
+                   style={{ width: '350px'}}>
+                <button type="button" onClick={() => { toggleModal(); }} className="absolute top-3 left-3">
+                  <img src="/litimg/back.png" alt="backbtn" className="w-7" />
+                </button>
+                <div className="flex flex-row items-center mt-3">
+                  <img src="/litimg/bug.png" alt="feedbackbtn" className="mr-2 w-5 h-full" />
+                  <div className="text-bgc1 font-bold text-lg">Report a Bug</div>
+                </div>
+                <button type="button" onClick={togglePModal} className="absolute top-3 right-3">
+                  <img src="/litimg/x.png" alt="xbtn" className="w-7" />
+                </button>
+                <div className="p-4 md:p-5 mb-5 text-center">
+                  <div className="flex flex-col"
+                       style={{ width: '320px'}}>
+                    <textarea
+                      placeholder={`Type your report here...`}
+                      value={reportText}
+                      style={{ height: '80px'}}
+                      className="border rounded-md py-2 px-2 mb-2 text-sm"
+                      onChange={(e) => {
+                        setReportText(e.target.value);
+                        setReportError('');
+                      }}
+                    />
+                    <button
+                      onClick={() => {
+                        if (!reportText.trim()) {
+                          setReportError('Please provide report before sending.');
+                          return;
+                        }
+                        handleSendReport();
+                        toggleSModal();
+                      }}
+                      type="button"
+                      className="px-5 py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer text-sm"
+                    >
+                      Send feedback
+                    </button>
+                    {reportError && (
+                      <div className="text-sm mt-2" style={{color: '#EE0000'}}>{reportError}</div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            id="popup-fmodal"
+            className={`modal ${isFModalVisible ? '' : 'hidden'}`}
+          >
+            <div className="w-full max-w-md max-h-full">
+              <div className="relative bg-white border border-white rounded-lg flex flex-col items-center"
+                   style={{ width: '350px'}}>
+                <button type="button" onClick={() => { toggleModal(); }} className="absolute top-3 left-3">
+                  <img src="/litimg/back.png" alt="backbtn" className="w-7" />
+                </button>
+                <div className="flex flex-row items-center mt-3">
+                  <img src="/litimg/idea.png" alt="feedbackbtn" className="mr-2 w-5 h-full" />
+                  <div className="text-bgc1 font-bold text-lg">Feedback</div>
+                </div>
+                <button type="button" onClick={toggleFModal} className="absolute top-3 right-3">
+                  <img src="/litimg/x.png" alt="xbtn" className="w-7" />
+                </button>
+                <div className="p-4 md:p-5 mb-5 text-center">
+                  <div className="flex flex-col"
+                       style={{ width: '320px'}}>
+                    <textarea
+                      placeholder={`Type your feedback here...`}
+                      value={feedbackText}
+                      style={{ height: '80px'}}
+                      className="border rounded-md py-2 px-2 mb-2 text-sm"
+                      onChange={(e) => {
+                        setFeedbackText(e.target.value);
+                        setFeedbackError('');
+                      }}
+                    />
+                    <button
+                      onClick={() => {
+                        if (!feedbackText.trim()) {
+                          setFeedbackError('Please provide feedback before sending.');
+                          return;
+                        }
+                        handleSendFeedback();
+                        toggleSModal();
+                      }}
+                      type="button"
+                      className="px-5 py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer text-sm"
+                    >
+                      Send feedback
+                    </button>
+                    {feedbackError && (
+                      <div className="text-sm mt-2" style={{color: '#EE0000'}}>{feedbackError}</div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            id="popup-smodal"
+            className={`modal ${isSModalVisible ? '' : 'hidden'}`}
+          >
+            <div className="w-full max-w-md max-h-full">
+              <div className="relative bg-white border border-white rounded-lg flex flex-col items-center"
+                   style={{ width: '350px' }}>
+                <button type="button" onClick={toggleSModal} className="absolute top-3 right-3">
+                  <img src="/litimg/x.png" alt="feedbacksbtn" className="w-7" />
+                </button>
+                <div className="p-4 md:p-5 mb-5 text-center">
+                  <div className="flex flex-col items-center">
+                    <img src="/litimg/success.png" alt="Success" className="w-11 mt-5 mb-3 h-full" />
+                    <div className="text-black font-semibold text-lg mb-6">We appreciate the feedback!</div>
+                    <button onClick={() => { toggleModal(); }}        
+                            type="button"
+                            className="px-5 py-2 rounded bg-bgc1 text-white font-semibold cursor-pointer text-sm">
+                      Send another feedback
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
         </div>
     </div>
